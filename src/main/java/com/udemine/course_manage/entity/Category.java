e4f08f2d0a5d78be8fe236b94efb7d27a05ed0fd
Lê Văn Trung <69235532+sevenghost-2001@users.noreply.github.com>
2025-06-22 00:40:23 +0700

    update design pattern category

--- src/main/java/com/udemine/course_manage/entity/Category.java
+++ src/main/java/com/udemine/course_manage/entity/Category.java
@@ -4,40 +4,23 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 import org.w3c.dom.Text;
 
 @Entity
 @Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Category {
     @Id
     @Column(nullable = false)
-    private int category_id;
+    private int id;
     @Column(nullable = false)
     private String name;
-    @Column(columnDefinition = "TEXT")
+    @Column(nullable = false)
     private String description;
-
-    public int getCategory_id() {
-        return category_id;
-    }
-
-    public void setCategory_id(int category_id) {
-        this.category_id = category_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
