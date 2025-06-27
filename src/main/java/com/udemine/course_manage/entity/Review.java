a34983daf7dd33f0e2f49b61b84fda1eabd5c64c
Lê Văn Trung <69235532+sevenghost-2001@users.noreply.github.com>
2025-06-27 22:15:34 +0700

    update entity Review

--- src/main/java/com/udemine/course_manage/entity/Review.java
+++ src/main/java/com/udemine/course_manage/entity/Review.java
@@ -1,5 +1,7 @@
 package com.udemine.course_manage.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -22,10 +24,26 @@ public class Review {
     LocalDateTime created_at;
     @ManyToOne
     @JoinColumn(name = "id_user", nullable = false)
+    @JsonIgnore//Ẩn đi user
     User user;
     @ManyToOne
     @JoinColumn(name = "id_course", nullable = false)
+    @JsonIgnore//Ẩn đi course
     Course course;
+
+    //Thêm 2 thuộc tính để trả về name và title
+    @Transient
+    @JsonProperty("Tên học viên")//Tên trong ngoặc kép muốn đặt sao cũng được
+    public String getUserName() {
+        return user != null ? user.getName() : null;
+    }
+
+    @Transient
+    @JsonProperty("Khóa học")
+    public String getCourseTitle() {
+        return course != null ? course.getTitle() : null;
+    }
+
     @PrePersist
     public void onCreate(){
         this.created_at = LocalDateTime.now();
